6751be84d83da5290ee2fbe561c90d8cb784df38
Luke Anthony <ln64.ohio@gmail.com>
2025-10-09 21:36:13 -0400

    deploy

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,16 +1,11 @@
-type GeoJSONFeatureCollection = {
-    type: string;
-    features: GeoJSONFeature[];
-};
-
 type GeoJSONFeature = {
-    type: string;
-    properties: {
-        name: string;
-        description: string;
-    };
-    geometry: {
-        type: string;
-        coordinates: [number, number][];
-    };
+	type: string;
+	properties: {
+		name: string;
+		description: string;
+	};
+	geometry: {
+		type: string;
+		coordinates: [number, number][];
+	};
 };
